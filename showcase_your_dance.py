e15c1dc04687a997af7dc08bff86c2289df0dae6
Roger-Block <150748225+Roger-Block@users.noreply.github.com>
2024-08-11 21:21:47 -0700

    Add files via upload

---
+++ showcase_your_dance.py
@@ -0,0 +1,60 @@
+#===============================================================
+#1. Showcase Your Dance Moves!
+	#Task 1: Code Correction
+print("="*50)
+weather = "sunny"
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+print("="*50)
+
+
+
+#===============================================================
+	#Task 2: Your Mood Today
+mood = input("How do you feel today? ")
+
+if mood == "happy":
+    print("That's great to hear!")
+elif mood == "sad":
+    print("I hope your day gets better!")
+else:
+    print("I'm sorry, I don't understand your mood.")
+
+
+
+
+#===============================================================
+#2. Python Data Types and type() Function
+    #Task 1: Code Correction
+print("="*50)
+
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+print(type(variable_a))
+print(type(variable_b))
+print(type(variable_c))
+print(type(variable_d))
+
+
+
+#===============================================================
+#3. Arithmetic Operations in Daily Life
+#Task 1: Grocery Store Math
+print("="*50)
+
+bread = 2.75
+peanut_butter = 4.50
+jelly = 3.23
+
+total_cost = bread + peanut_butter + jelly
+print("Bread, peanut butter and jelly will be:")
+print("Total cost:", total_cost,"!!!!")
+
+
+print("="*50)
+#================================================
